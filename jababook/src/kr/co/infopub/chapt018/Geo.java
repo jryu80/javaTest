3a46883e015b99040d3871acd5a1b0a23f0d76e7
jryu80 <jaehak.ryu80@gmail.com>
2023-05-17 15:02:04 +0900

    1

---
+++ jababook/src/kr/co/infopub/chapt018/Geo.java
@@ -0,0 +1,9 @@
+package kr.co.infopub.chapt018;
+
+public class Geo {
+	//클래스에서 멤버변수를 선언하면 초기화를 안해도 자동초기화가 된다.
+	public double latitude2;
+	public double longitude2;
+	
+
+}
---
+++ jababook/src/kr/co/infopub/chapt018/GeoMain.java
@@ -0,0 +1,9 @@
+package kr.co.infopub.chapt018;
+
+public class GeoMain {
+	public static void main(String[] args) {
+		
+		System.out.printf("@@@@@@@@@@@@@@@@");
+	}
+
+}
